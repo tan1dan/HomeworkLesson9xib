6833364b9318444e4797a7a5a653a871e3caf2fc
Ivan Znak <ivanznak04@gmail.com>
2023-11-12 14:37:41 +0100

    Project was added

---
+++ HomeworkLesson9xib/HomeworkLesson9xib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeworkLesson9xib
+//
+//  Created by Иван Знак on 12/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

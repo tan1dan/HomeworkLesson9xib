e93cc01cf01359e915d74be6d85b3ee88a294bc9
Ivan Znak <ivanznak04@gmail.com>
2023-11-12 23:59:45 +0100

    Logic was added

--- HomeworkLesson9xib/HomeworkLesson9xib/FirstView.swift
+++ HomeworkLesson9xib/HomeworkLesson9xib/FirstView.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class FirstView: UIView {
+class FirstView: UIView, UITextFieldDelegate {
+    
+    var textClosure : ((String) -> Void)?
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        print(label1)
+        label1?.text = "First"
+        commonInit()
+        print(textField1)
+        textField1?.delegate = self
+        textField2?.delegate = self
+        textField3?.delegate = self
+        
     }
     
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        
+    }
+    private func commonInit(){
+        let xib = UINib(nibName: "FirstView", bundle: nil)
+        let view = xib.instantiate(withOwner: self).first as! FirstView
+        
+        view.frame = self.bounds
+        addSubview(view)
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        textClosure?(textField.text ?? "")
+        return true
     }
 }
--- HomeworkLesson9xib/HomeworkLesson9xib/ViewController.swift
+++ HomeworkLesson9xib/HomeworkLesson9xib/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let customView = FirstView(frame: .zero)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(customView)
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            customView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            customView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            customView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        customView.textClosure = {string in
+            print(string)
+        }
     }
 
 

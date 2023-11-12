f05645bbc2b908766eca8dc06ea98226de852afe
Ivan Znak <ivanznak04@gmail.com>
2023-11-12 14:47:23 +0100

    FirstView was created

---
+++ HomeworkLesson9xib/HomeworkLesson9xib/FirstView.swift
@@ -0,0 +1,19 @@
+//
+//  FirstView.swift
+//  HomeworkLesson9xib
+//
+//  Created by Иван Знак on 12/11/2023.
+//
+
+import UIKit
+
+class FirstView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
